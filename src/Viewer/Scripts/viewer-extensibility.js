42da2cc4492cb0903e514250aadfe1fedfc7ca45
BimCadDeveloper <cadbimdeveloper@yandex.ru>
2017-11-14 17:18:03 +0300

    create very basic viewer extension

---
+++ src/Viewer/Scripts/viewer-extensibility.js
@@ -0,0 +1,23 @@
+﻿function ModelNotes(viewer, options) {
+    Autodesk.Viewing.Extension.call(this, viewer, options);
+
+    var self = this;
+
+    self.load = function () {
+        alert("extension loaded");
+
+        return true;
+    };
+
+    self.unload = function () {
+        Autodesk.Viewing.theExtensionManager.unregisterExtension("ModelNotes");
+        alert("extension unloaded");
+
+        return true;
+    };
+}
+
+ModelNotes.prototype = Object.create(Autodesk.Viewing.Extension.prototype);
+ModelNotes.prototype.constructor = ModelNotes;
+
+Autodesk.Viewing.theExtensionManager.registerExtension("ModelNotes", ModelNotes);
